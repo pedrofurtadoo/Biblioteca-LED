2ae1090a8ec02e441615fc846f47ecc90915ec9c
PEDRO STAIN FURTADO <46161394820@info.local>
2026-04-14 08:54:58 -0300

    First commit

---
+++ src/LED.cpp
@@ -0,0 +1,126 @@
+#include "LED.h"
+
+Led::Led(uint8_t pin) :
+    pino(pin),
+    estado(LOW),
+    desligarPorTempo(false),
+    desligarNoMomento(0),
+    tempoAnteriorPiscar(0),
+    tempoEspera(0),
+    estadoPiscar(false),
+    repeticoes()
+{
+    pinMode(pino, OUTPUT);
+}
+
+void Led::ligar()
+{
+    estadoPiscar = false;
+    desligarPorTempo = false;
+    estado = HIGH;
+}
+
+void Led::ligar(uint32_t tempoLigado_ms)
+{
+    estado = HIGH;
+    desligarPorTempo = true;
+    desligarNoMomento = millis() + tempoLigado_ms;
+}
+
+void Led::desligar()
+{
+    estado = LOW;
+}
+
+void Led::alternar()
+{
+    estado = !estado;
+}
+
+void Led::update()
+{
+    if (desligarPorTempo)
+    {
+        funcaoDesligamento();
+    }
+
+    if (estadoPiscar)
+    {
+        funcaoPiscar();
+    }
+
+    digitalWrite(pino, estado);
+}
+
+bool Led::getEstado()
+{
+    return estado;
+}
+
+uint8_t Led::getPino()
+{
+    return pino;
+}
+
+void Led::setEstado(bool estado)
+{
+    this->estado = estado;
+    desligarPorTempo = false;
+    estadoPiscar = false;
+}
+
+void Led::piscar()
+{
+    estadoPiscar = true;
+    tempoEspera = 500;
+    tempoAnteriorPiscar = millis();
+    estado = HIGH;
+    desligarPorTempo = false;
+}
+
+void Led::piscar(float frequencia)
+{
+    estadoPiscar = true;
+    tempoEspera = (1000.0f / 2.0f * frequencia);
+    tempoAnteriorPiscar = millis();
+    estado = HIGH;
+    desligarPorTempo = false;
+}
+
+void Led::piscar(float frequencia, uint16_t repeticoes)
+{
+    estadoPiscar = true;
+    tempoEspera = (1000.0f / 2.0f * frequencia);
+    tempoAnteriorPiscar = millis();
+    estado = HIGH;
+    this->repeticoes = repeticoes * 2;
+    desligarPorTempo = false;
+}
+
+void Led::funcaoPiscar()
+{
+    if (millis() - tempoAnteriorPiscar >= tempoEspera)
+    {
+        estado = !estado;
+        tempoAnteriorPiscar = millis();
+
+        if (repeticoes > 0)
+        {
+            repeticoes--;
+            if (repeticoes == 0)
+            {
+                estadoPiscar = false;
+                estado = LOW;
+            }
+        }
+    }
+}
+
+void Led::funcaoDesligamento()
+{
+    if (millis() >= desligarNoMomento)
+    {
+        estado = LOW;
+        desligarPorTempo = false;
+    }
+}
---
+++ src/LED.h
@@ -0,0 +1,53 @@
+#ifndef LED_h
+#define LED_h
+
+#include <Arduino.h>
+
+class Led
+{
+    private:
+        uint8_t pino;
+        bool estado;
+        bool desligarPorTempo = false;
+        uint32_t desligarNoMomento;
+
+        uint32_t tempoAnteriorPiscar;
+        uint32_t tempoEspera;
+        bool estadoPiscar;
+        uint16_t repeticoes;
+        void funcaoPiscar();
+        void funcaoDesligamento();
+
+
+
+    
+
+    public:
+        Led(uint8_t pin); //Método construtor, deverá ter o mesmo nome da classes
+
+        void ligar();
+        void ligar(uint32_t tempoLigado_ms);
+
+        void desligar();
+
+        void piscar();
+        void piscar(float frequencia);
+        void piscar(float frequencia, uint16_t repeticoes);
+
+        bool getEstado();
+        uint8_t getPino();
+
+        void setEstado(bool estado);
+
+        void alternar();
+
+        void update();
+
+        
+
+
+
+
+};
+
+#endif
